e35d0d2516a33d7401cf612ca158a091e7f508e0
carrollsa <stephenalexandercarroll@gmail.com>
2022-03-14 14:48:30 -0400

    Modify about

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < ApplicationController
   end
 
   def about
+    @welcome = "Welcome to RailsFriends!"
+    @info = "This is a Ruby on Rails demo app where you can perform basic CRUD operations on a list of friends for individual users."
   end
 end
